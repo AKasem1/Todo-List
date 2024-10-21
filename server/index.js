ca9b7718d064f061e6c9adc280d88f51bcbd0c22
CodeCantaloupe <141568278+CodeCantaloupe@users.noreply.github.com>
2024-10-22 00:47:24 +0300

    initial setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+
+/*
+    To get started first "cd server" and install the necessary dependencies
+    like express and optionally mongodb/mongoose (if we're going to use it)
+
+    also install nodemon if you want to use it but it's optional
+    if you decide to use it, run "nodemon index.js".
+
+    else just run "node index.js"
+    (but it will not watch for any changes in the code)
+*/
+
+// const express = require("express");
+// const app = express();
+// app.use(express.json());
