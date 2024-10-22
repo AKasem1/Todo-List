1e6b73297211bbfbb723166bb81231cb01e2a2ba
CodeCantaloupe <141568278+CodeCantaloupe@users.noreply.github.com>
2024-10-22 21:54:21 +0300

    Added onTaskStatusChange functionality to TaskListView

--- client/src/components/TaskItem.js
+++ client/src/components/TaskItem.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const TaskItem = ({state}) => {
-  const checkboxState = (state == "complete") ? true : false;
-  const taskNameState = (state == "complete") ? "line-through text-gray-500" : "";
+const TaskItem = ({ taskId, status, taskName, onTaskStatusChange }) => {
+  const checkboxState = (status == "complete") ? true : false;
+  const taskNameState = (status == "complete") ? "line-through text-gray-500" : "";
 
   return (
     <div className='group flex items-center p-6'>
-        <input type="checkbox" className='mr-6 size-6' checked={checkboxState}/>
-        <p className={`text-2xl flex-grow ${taskNameState}`}>Task</p>
-        <button className='invisible group-hover:visible bg-white p-2 rounded-full hover:bg-red-600 transition-colors'>
-            <img src='/trashcan.svg'/>
-        </button>
+      <input type="checkbox" className='mr-6 size-6' defaultChecked={checkboxState} onChange={() => onTaskStatusChange(taskId)} />
+      <p className={`text-2xl flex-grow ${taskNameState}`}>{taskName}</p>
+      <button className='invisible group-hover:visible bg-white p-2 rounded-full hover:bg-red-600 transition-colors'>
+        <img src='/trashcan.svg' />
+      </button>
     </div>
   )
 }
--- client/src/components/TasksListView.js
+++ client/src/components/TasksListView.js
@@ -1,29 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TaskItem from './TaskItem'
 
 const TasksListView = () => {
-  const tasks = [1, 2]
+  const [allTasks, setAllTasks] = useState([
+    {
+      _id: 1,
+      name: "Task 1",
+      status: "pending"
+    },
+    {
+      _id: 2,
+      name: "Task 2",
+      status: "complete"
+    },
+    {
+      _id: 3,
+      name: "Task 3",
+      status: "pending"
+    }
+  ]);
+
+  const [pendingTasks, setPendingTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
+
+  useEffect(() => {
+    setPendingTasks(allTasks.filter(task => task.status === "pending"));
+    setCompletedTasks(allTasks.filter(task => task.status === "complete"));
+  }, [allTasks]);
+
+  const onTaskStatusChange = (taskId) => {
+    setAllTasks(prevTasks =>
+      prevTasks.map(task =>
+        task._id === taskId ? { ...task, status: task.status === "pending" ? "complete" : "pending" } : task
+      )
+    );
+  }
 
   return (
-    <div className='bg-white border rounded-lg shadow-xl mx-auto w-[80%]'>
+    <div className='bg-white border rounded-lg shadow-xl mx-auto w-[80%] py-6'>
 
-      <div className="flex items-center justify-center w-full mt-4">
+      <div className="flex items-center justify-center w-full">
         <hr className="border-1 flex-grow border-gray-300 mx-4" />
         <h1 className="text-xl text-center text-red-500 mx-4 underline whitespace-nowrap">Pending Tasks</h1>
         <hr className="border-1 flex-grow border-gray-300 mx-4" />
       </div>
 
-      {tasks.map((task, index) => {
-        return (<TaskItem key={index} className="first:rounded-t-lg last:rounded-b-lg not:first:not:last:border-b" />)
+      {pendingTasks.map((task) => {
+        return (<TaskItem key={task._id} id={task._id} status={task.status} taskName={task.name} onTaskStatusChange={() => onTaskStatusChange(task._id)} />)
       })}
 
-      <div className="flex items-center justify-center w-full mt-4">
+      <div className="flex items-center justify-center w-full">
         <hr className="border-1 flex-grow border-gray-300 mx-4" />
         <h1 className="text-xl text-center text-green-500 mx-4 underline whitespace-nowrap">Completed Tasks</h1>
         <hr className="border-1 flex-grow border-gray-300 mx-4" />
       </div>
-      {tasks.map((task, index) => {
-        return (<TaskItem key={index} state="complete" className="first:rounded-t-lg last:rounded-b-lg not:first:not:last:border-b" />)
+
+      {completedTasks.map((task) => {
+        return (<TaskItem key={task._id} id={task._id} status={task.status} taskName={task.name} onTaskStatusChange={() => onTaskStatusChange(task._id)} />)
       })}
     </div>
   )
