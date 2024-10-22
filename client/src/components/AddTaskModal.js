8b7c4bce4c8a577e2b96b58df6a81109f430828a
CodeCantaloupe <141568278+CodeCantaloupe@users.noreply.github.com>
2024-10-22 03:48:59 +0300

    Modified AddTaskModal UI

--- client/src/components/AddTaskModal.js
+++ client/src/components/AddTaskModal.js
@@ -4,11 +4,19 @@ const AddTaskModal = ({ onAddTaskHandler }) => {
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
             <div className="bg-white rounded-lg p-6 w-1/3">
-                <h2 className="text-xl font-bold mb-4">This is a Modal</h2>
-                <p className="mb-4">You can add any content you want here.</p>
-                <button onClick={onAddTaskHandler} className="px-4 py-2 bg-red-500 text-white rounded-lg">
-                    Close Modal
-                </button>
+                <h2 className="text-xl mb-6">Add a Task</h2>
+
+                <label>Task name:</label>
+                <input type='text' placeholder='واحد فول و اتنين طعمية' className="w-full p-2 border border-gray-300 rounded-lg mb-6"/>
+
+                <div className='space-x-4'>
+                    <button onClick={onAddTaskHandler} className="px-4 py-2 bg-green-500 text-white rounded-lg">
+                        Add Task
+                    </button>
+                    <button onClick={onAddTaskHandler} className="px-4 py-2 bg-red-500 text-white rounded-lg">
+                        Cancel
+                    </button>
+                </div>
             </div>
         </div>
     )
